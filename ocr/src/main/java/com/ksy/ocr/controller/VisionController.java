9e1ae6c41b2febace0920e898222510dd8503664
sykim <sykim@skyware.co.kr>
2024-07-25 10:43:35 +0900

    feat: img pdf 구분

--- ocr/src/main/java/com/ksy/ocr/controller/VisionController.java
+++ ocr/src/main/java/com/ksy/ocr/controller/VisionController.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,17 +20,19 @@ public class VisionController {
 
     private final VisionService visionService;
 
-    @PostMapping("/export2")
-    public String export2(List<MultipartFile> imgFile) throws IOException {
-        System.out.println(imgFile.get(0).getContentType());
+    @PostMapping("/export")
+    public String export(List<MultipartFile> imgFile) throws IOException {
+        String contentType = Objects.requireNonNull(imgFile.get(0).getContentType(), "File not exist").split("/")[0];
         List<byte[]> uploadImgList = new ArrayList<>();
+
         for(MultipartFile file : imgFile) {
             byte[] imgBytes = file.getBytes();
             uploadImgList.add(imgBytes);
         }
 
         try {
-            return visionService.getTextFromPdf(uploadImgList);
+            return contentType.equals("image") ?
+                    visionService.getTextFromImg(uploadImgList) : visionService.getTextFromPdf(uploadImgList);
         } catch (Exception e) {
             //e.printStackTrace();
             return "Failed to extract text: " + e.getMessage();
@@ -39,7 +42,10 @@ public class VisionController {
 
     @GetMapping("/extract")
     public String extract() {
-
+        /*
+        * TODO --
+        * 이미지인 경우 필요 요수 추출
+         */
         List<String> n = Arrays.asList("jo", "ko", "ja", "[사업자]: 536-37-00183");
 
         String a = n.stream().filter(x->x.contains("사업자")).collect(Collectors.joining(""));
